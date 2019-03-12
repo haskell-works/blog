e98e7d8592cdb4e55b700db3b2cb7a7f8d973196
John Ky <newhoggy@gmail.com>
2019-03-13 00:15:54 +1100

    Flexible colours

--- app/Blog/Diagrams/Json/StateRailroad.hs
+++ app/Blog/Diagrams/Json/StateRailroad.hs
@@ -25,10 +25,10 @@ transitionFor c =
   )
 
 transitionDiagram :: Char -> (State, State, State, State) -> Diagram B
-transitionDiagram c (InJson  , InJson  , InString, InString) = transition1 [c]
-transitionDiagram c (InString, InString, InJson  , InString) = transition2 [c]
-transitionDiagram c (InValue , InValue , InString, InString) = transition3 [c]
-transitionDiagram c (InJson  , InJson  , InEscape, InString) = transition4 [c]
+transitionDiagram c (InJson  , InJson  , InString, InString) = transition1 (repeat green ) [c]
+transitionDiagram c (InString, InString, InJson  , InString) = transition2 (repeat orange) [c]
+transitionDiagram c (InValue , InValue , InString, InString) = transition3 (repeat brown ) [c]
+transitionDiagram c (InJson  , InJson  , InEscape, InString) = transition4 (repeat blue  ) [c]
 transitionDiagram c _                                        = transition0 [c]
 
 selectTransition :: Char -> Diagram B
@@ -41,10 +41,10 @@ eachTransition :: Diagram B
 eachTransition = vsep 1
   [ hsep 1
     [ legend
-    , transition1 "A"
-    , transition2 "B"
-    , transition3 "C"
-    , transition4 "D"
+    , transition1 (repeat green ) "A"
+    , transition2 (repeat orange) "B"
+    , transition3 (repeat brown ) "C"
+    , transition4 (repeat blue  ) "D"
     ] # scale 2 # center
   ] # withName "in-escape" (\s d -> d # fc blue)
   where legend = mconcat
@@ -77,52 +77,52 @@ transition0 label = mconcat
   , text label # translateX 1.5 # translateY (-1.5) # font "Consolas,monaco,monospace" # fc black
   ]
 
-transition1 :: String -> QDiagram B V2 Double Any
-transition1 label = mconcat
+transition1 :: [Colour Double] -> String -> QDiagram B V2 Double Any
+transition1 cs label = mconcat
   [ vrule 9 # lc silver # translateY 2
   , vrule 9 # lc silver # translateY 2 # translateX 3
-  , [ smSingleTransition 3 2 # lc green
-    , smSingleTransition 2 2 # lc green
-    , smSingleTransition 1 0 # lc green
-    , smSingleTransition 0 0 # lc green
+  , [ smSingleTransition 3 2 # lc (cs !! 3)
+    , smSingleTransition 2 2 # lc (cs !! 2)
+    , smSingleTransition 1 0 # lc (cs !! 1)
+    , smSingleTransition 0 0 # lc (cs !! 0)
     ] # mconcat # opacityGroup 0.5
-  , text label # translateX 1.5 # translateY (-1.5) # font "Consolas,monaco,monospace" # fc green
+  , text label # translateX 1.5 # translateY (-1.5) # font "Consolas,monaco,monospace" # fc (cs !! 4)
   ]
 
-transition2 :: String -> QDiagram B V2 Double Any
-transition2 label = mconcat
+transition2 :: [Colour Double] -> String -> QDiagram B V2 Double Any
+transition2 cs label = mconcat
   [ vrule 9 # lc silver # translateY 2
   , vrule 9 # lc silver # translateY 2 # translateX 3
-  , [ smSingleTransition 3 2 # lc orange
-    , smSingleTransition 2 0 # lc orange
-    , smSingleTransition 1 2 # lc orange
-    , smSingleTransition 0 2 # lc orange
+  , [ smSingleTransition 3 2 # lc (cs !! 3)
+    , smSingleTransition 2 0 # lc (cs !! 2)
+    , smSingleTransition 1 2 # lc (cs !! 1)
+    , smSingleTransition 0 2 # lc (cs !! 0)
     ] # mconcat # opacityGroup 0.5
-  , text label # translateX 1.5 # translateY (-1.5) # font "Consolas,monaco,monospace" # fc orange
+  , text label # translateX 1.5 # translateY (-1.5) # font "Consolas,monaco,monospace" # fc (cs !! 4)
   ]
 
-transition3 :: String -> QDiagram B V2 Double Any
-transition3 label = mconcat
+transition3 :: [Colour Double] -> String -> QDiagram B V2 Double Any
+transition3 cs label = mconcat
   [ vrule 9 # lc silver # translateY 2
   , vrule 9 # lc silver # translateY 2 # translateX 3
-  , [ smSingleTransition 3 2 # lc brown
-    , smSingleTransition 2 2 # lc brown
-    , smSingleTransition 1 1 # lc brown
-    , smSingleTransition 0 1 # lc brown
+  , [ smSingleTransition 3 2 # lc (cs !! 3)
+    , smSingleTransition 2 2 # lc (cs !! 2)
+    , smSingleTransition 1 1 # lc (cs !! 1)
+    , smSingleTransition 0 1 # lc (cs !! 0)
     ] # mconcat # opacityGroup 0.5
-  , text label # translateX 1.5 # translateY (-1.5) # font "Consolas,monaco,monospace" # fc brown
+  , text label # translateX 1.5 # translateY (-1.5) # font "Consolas,monaco,monospace" # fc (cs !! 4)
   ]
 
-transition4 :: String -> QDiagram B V2 Double Any
-transition4 label = mconcat
+transition4 :: [Colour Double] -> String -> QDiagram B V2 Double Any
+transition4 cs label = mconcat
   [ vrule 9 # lc silver # translateY 2
   , vrule 9 # lc silver # translateY 2 # translateX 3
-  , [ smSingleTransition 3 2 # lc blue
-    , smSingleTransition 2 3 # lc blue
-    , smSingleTransition 1 0 # lc blue
-    , smSingleTransition 0 0 # lc blue
+  , [ smSingleTransition 3 2 # lc (cs !! 3)
+    , smSingleTransition 2 3 # lc (cs !! 2)
+    , smSingleTransition 1 0 # lc (cs !! 1)
+    , smSingleTransition 0 0 # lc (cs !! 0)
     ] # mconcat # opacityGroup 0.5
-  , text label # translateX 1.5 # translateY (-1.5) # font "Consolas,monaco,monospace" # fc blue
+  , text label # translateX 1.5 # translateY (-1.5) # font "Consolas,monaco,monospace" # fc (cs !! 4)
   ]
 
 jsonText :: String
